44fac8fd51d12a3fcb83f965326b56121478bf96
augusstt06 <augusstt06@gmail.com>
2022-03-09 22:46:17 +0900

    Study : Solving Greedy Problems_BaekJoon 11399, 11047, 1931, 13305

--- Algorithm_2_greedy/greedy_practice1.py
+++ Algorithm_2_greedy/greedy_practice1.py
@@ -92,6 +92,28 @@
 
 
 # 백준 1541번
-n = input()
+# n = input()
 
 # 백준 13305번
+n, k = input(), list(map(int, input().split()))
+price = list(map(int, input().split()))
+# print(k, price)
+min_p = price[0]
+total_price = 0
+total_far = 0
+
+for i in range(len(price) - 1):
+    if min_p > price[i]:
+        min_p = price[i]
+for v in k:
+    total_far += v
+
+for j in range(len(k)):
+    if price[j] != min_p:
+        total_price += k[j] * price[j]
+        total_far -= k[j]
+    else:
+        total_price += total_far * price[j]
+        break
+
+print(total_price)
