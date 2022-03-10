c88dd58b2d1b4f5830da040fd717ba4e021a357c
augusstt06 <augusstt06@gmail.com>
2022-03-10 14:57:42 +0900

    Study : Solving Greedy Problems_BaekJoon 1541

--- Algorithm_2_greedy/greedy_practice1.py
+++ Algorithm_2_greedy/greedy_practice1.py
@@ -93,27 +93,42 @@
 
 # 백준 1541번
 # n = input()
-
+# num = ''
+# answer = 0
+# for i in n:
+#     if i != '-' and i != '+':
+#         num += i
+#     elif i == '-':
+#         answer += int(num)
+#         num = '-'
+#     elif i == '+' and num[0] == '-':
+#         answer += int(num)
+#         num = '-'
+#     elif i == '+' and num[0] != '-':
+#         answer += int(num)
+#         num = ''
+# answer += int(num)
+# print(answer)
 # 백준 13305번
-n, k = input(), list(map(int, input().split()))
-price = list(map(int, input().split()))
+# n, k = input(), list(map(int, input().split()))
+# price = list(map(int, input().split()))
 # print(k, price)
-min_p = price[0]
-total_price = 0
-total_far = 0
-
-for i in range(len(price) - 1):
-    if min_p > price[i]:
-        min_p = price[i]
-for v in k:
-    total_far += v
-
-for j in range(len(k)):
-    if price[j] != min_p:
-        total_price += k[j] * price[j]
-        total_far -= k[j]
-    else:
-        total_price += total_far * price[j]
-        break
-
-print(total_price)
+# min_p = price[0]
+# total_price = 0
+# total_far = 0
+#
+# for i in range(len(price) - 1):
+#     if min_p > price[i]:
+#         min_p = price[i]
+# for v in k:
+#     total_far += v
+#
+# for j in range(len(k)):
+#     if price[j] != min_p:
+#         total_price += k[j] * price[j]
+#         total_far -= k[j]
+#     else:
+#         total_price += total_far * price[j]
+#         break
+#
+# print(total_price)
